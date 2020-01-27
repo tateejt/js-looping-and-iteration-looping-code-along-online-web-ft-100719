3df761a8188bd970e5c1b470bb258da2382e6703
Emery Tate <tateejt@gmail.com>
2020-01-27 15:40:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solutions in this file
+const cards = ["Lisa", "Kaitlin", "Jan"]
+function writeCards(cards) {
+  for (let i = 0; i < cards.length; i++) {
+    console.log(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+  }
+  
+  return cards;
+}
+
+writeCards(cards);
